65143510b6b1f291cc34a7c93cf44a629351e88c
Seth Adams <seth814@vt.edu>
2018-10-21 04:09:45 -0400

    added imports

---
+++ model.py
@@ -0,0 +1,12 @@
+import os
+from scipy.io import wavfile
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from keras.layers import Conv2D, MaxPool2D, Flatten, LSTM
+from keras.layers import Dropout, Dense, TimeDistributed
+from keras.models import Sequential
+from keras.utils import to_categorical
+from sklearn.utils.class_weight import compute_class_weight
+from tqdm import tqdm
+from python_speech_features import mfcc
